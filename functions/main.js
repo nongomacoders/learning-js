67ff1364d3c295e15be36d1cec0e72c116fda9ef
bongiwentshangase <bongiwe.buselaphi@gmail.com>
2020-11-17 16:35:29 +0200

    function

---
+++ functions/main.js
@@ -0,0 +1,13 @@
+function hello(fn,sn){
+   console.log(`hello ${fn} ${sn}`);
+}
+
+hello("bongiwe","ntshagase");   // you need to call the funtion or execute the function for it to work
+hello("mesuli","mkhonza");
+
+
+function intro(fn,sn,age){
+console.log(`my name is ${fn} ${sn} i am ${age} years old`);
+}
+intro("bongiwe","ntshagase",21);
+
--- strings/main.js
+++ strings/main.js
@@ -20,15 +20,23 @@ let caps = fn.toUpperCase();   // caps = BONGIWE
 
 //get part of a string
 fullname = "bongiwe ntshangase";
+
  //  substring. this gets part of the string . the first number is start index and the second is end  index but not including end
 let partname = fullname.substring(0, 4);  //partname=bong
 partname = fullname.substring(1, 3);   //partname=on
-partname=fullname.substring(3)   //partname=giwe ntshangase . if you dont put end number 
+partname=fullname.substring(3)   //partname=giwe ntshangase . 
+//if you dont put end number,it goes from the first number to the end of the string
 
 let i = fullname.indexOf("b");  // i= 0
 i = fullname.indexOf(" ");  //i=7
 fn = fullname.substring(0, 7);   //fn = bongiwe
-sn = fullanme.substring(8);         //sn=ntshangase
-sn = fullname.substring()
+sn = fullname.substring(8);         //sn=ntshangase
+let fullname2= "mesuli mkhonza"
+let i2= fullname2.indexOf(" "); //i2=6
+let fn2= fullname2.substring(0,i2);
+let sn2=fullname2.substring(i2+1);
+console.log(`fn2 = ${fn2}`)
+console.log(`sn2 = ${sn2}`)
+
 
 
