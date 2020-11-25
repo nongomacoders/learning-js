4b8a496ea07b181deee5bccbc29b3dbb0330c922
asusdion <dr.dtdavan@gmail.com>
2020-11-25 14:18:43 +0200

    loops

---
+++ arrays/main.js
@@ -0,0 +1,11 @@
+let days = ["mon", "tues", "wed", "thurs", "fri", "sat", "sun"];
+
+console.log(days[0]);
+
+let weekend = days.slice(5, 7);
+console.log(weekend);
+
+let iThurs = days.indexOf("thurs");
+console.log(iThurs);
+
+console.log(days.length);
---
+++ loops/main.js
@@ -0,0 +1,17 @@
+/* for (let x = 0; x <= 10; x++){
+    console.log(x)
+} */
+
+
+let days = ["mon", "tues", "wed", "thurs", "fri", "sat", "sun"];
+
+/* for (let y = 0; y < days.length; y++){
+    console.log(days[y])
+} */
+
+days.forEach(
+    (day) => console.log(day)
+)
+
+
+
